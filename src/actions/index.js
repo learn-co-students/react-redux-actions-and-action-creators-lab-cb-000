bb0d4d42cbabcdfb976ad1c03776428a6e923945
Jason Vance <jbvance@gmail.com>
2017-12-26 19:47:09 -0600

    complete lab

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,28 @@
 // define your actions here!
+export function addMovie(movie) {
+    return {
+        type: 'ADD_MOVIE',
+        movie
+    };
+}
+
+export function deleteMovie(movie) {
+    return {
+        type: 'DELETE_MOVIE',
+        movie
+    };
+}
+
+export function updateUsername(username) {
+    return {
+        type: 'UPDATE_USERNAME',
+        username
+    }
+}
+
+export function resetUsername(username) {
+    return {
+        type: 'RESET_USERNAME',
+        username: 'default'
+    };
+}
